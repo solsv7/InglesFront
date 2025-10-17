2f7a71cfb7a71ddd17f63236c36f0899db7ebc1a
Lauuti19 <lautibustamante.lomb@gmail.com>
2025-10-17 20:18:19 -0300

    cambio para el login use headers

--- src/components/HomePageComponents/Login/LoginComponent.js
+++ src/components/HomePageComponents/Login/LoginComponent.js
@@ -20,7 +20,15 @@ const LoginComponent = () => {
         setLoading(true);
         
         try {
-            const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { dni, password });
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, 
+                { dni, password },
+                {
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            );
             const { token, user } = response.data;
 
             localStorage.setItem('token', token);
